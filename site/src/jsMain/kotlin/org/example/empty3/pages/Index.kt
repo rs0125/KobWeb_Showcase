03b38ff8eb326b53c6bd37bd12e900aa509b7aec
Raghav Sharma <raghaaav5@gmail.com>
2024-11-05 01:12:39 +0530

    feat: Added a basic input state management demonstration

--- site/src/jsMain/kotlin/org/example/empty3/pages/Index.kt
+++ site/src/jsMain/kotlin/org/example/empty3/pages/Index.kt
@@ -1,18 +1,72 @@
 package org.example.empty3.pages
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSmile
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.icons.fa.IconStyle
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.placeholder
+import org.jetbrains.compose.web.css.LineStyle
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Input
+import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
+import org.w3c.dom.Attr
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    var name by remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            FaFaceSmile(
+                modifier = Modifier.margin(right=10.px),
+                style = IconStyle.FILLED,
+                size = IconSize.XXL
+            )
+            P(
+                attrs = Modifier
+                    .fontSize(40.px)
+                    .toAttrs()
+            ){
+                Text("Hello, $name")
+            }
+        }
+        Input(
+
+            type = InputType.Text,
+            attrs = Modifier
+                .padding ( 10.px )
+                .margin (10.px)
+                .outline(
+                    width = 0.px,
+                    style = LineStyle.None,
+                    color = Colors.Transparent
+                )
+                .toAttrs {
+                placeholder("Enter Name")
+                    onInput {
+                        name = it.value
+                    }
+                },
+
+        )
     }
 }
